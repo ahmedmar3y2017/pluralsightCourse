88fb39e7a5a3d24580b9fb94767bc3ca3374c92e
ahmed mar3y <ahmedmar3y108@gmail.com>
2018-04-25 13:20:01 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var message="ahmed mohamed mar3y";
+
+console.log(message)
+
+var result = document.getElementById("result");
+result.innerHTML = "This is My Js File ";
+
+console.log("Type of : "+ typeof(message));
+console.log("Type of : "+ typeof(result));
+
+
